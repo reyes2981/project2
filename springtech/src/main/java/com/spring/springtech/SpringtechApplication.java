78285e0f67086dffd134d35b71e020ba9c906c45
noe <noe.reyes92@yahoo.com>
2022-01-29 13:16:01 -0600

    first commit

---
+++ springtech/src/main/java/com/spring/springtech/SpringtechApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtechApplication.class, args);
+    }
+
+}
---
+++ springtech/src/test/java/com/spring/springtech/SpringtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

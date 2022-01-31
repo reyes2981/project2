9b38f5f803dc65f9a75a118deea7e48766061aaa
noe <noe.reyes92@yahoo.com>
2022-01-31 09:36:26 -0600

    added put/delete functionality to RoleController

---
+++ springtech/README.md
@@ -0,0 +1,3 @@
+types of roles
+admin
+jobseeker
--- springtech/src/main/java/com/spring/springtech/controller/RoleController.java
+++ springtech/src/main/java/com/spring/springtech/controller/RoleController.java
@@ -3,15 +3,15 @@ package com.spring.springtech.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping("/api")
 public class RoleController {
 
-    @GetMapping(path = "/hello-world/")
+    @GetMapping("/hello-world/")
     public String getHelloWorld() {
         return "Hello World!";
     }
 
-    @GetMapping(path = "/roles/")
+    @GetMapping("/roles/")
     public String getAllRoles() {
         return "all roles";
     }
@@ -19,6 +19,21 @@ public class RoleController {
     @PostMapping("/roles/")
     public String createRole(@RequestBody String body) {
         return "creating a role " + body;
+    }
+
+    @GetMapping("/roles/{roleId}/")
+    public String getRole(@PathVariable Long roleId) {
+        return "getting the category with id of " + roleId;
+    }
+
+    @PutMapping("/roles/{roleId}/")
+    public String updateRole(@PathVariable(value = "roleId") Long roleId, @RequestBody String body) {
+        return "updating the category with id of " + roleId + body;
+    }
+
+    @DeleteMapping ("/roles/{roleId}/")
+    public String deleteRole(@PathVariable(value = "roleId") Long roleId) {
+        return "deleting the role with id of " + roleId;
     }
 
 }

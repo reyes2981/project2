da475dad1bd6685dbe987937963bd8c344c992a5
noe <noe.reyes92@yahoo.com>
2022-01-31 11:52:42 -0600

    refactored User model, next going to work on user controller

--- springtech/src/main/java/com/spring/springtech/model/User.java
+++ springtech/src/main/java/com/spring/springtech/model/User.java
@@ -1,8 +1,12 @@
 package com.spring.springtech.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -10,21 +14,68 @@ public class User {
 
     @Id
     @Column
+    //DB will automatically create an ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String username;
+    //makes
+    @Column(unique = true)
+    private String userName;
+
+    @Column(unique = true)
+    private String emailAddress;
 
     @Column
-    private boolean isPublic;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Role role;
+    // one user can have only one profile
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "profile_id",referencedColumnName = "id")
+//    private UserProfile userProfile;
+
+//    // user can have more than one recipe
+//    @OneToMany(mappedBy = "user")
+//    @LazyCollection(LazyCollectionOption.FALSE)
+//    private List<Recipe> recipeList;
+
+    // user can have more than one category
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Role> roleList;
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+
+//    public List<Recipe> getRecipeList() {
+//        return recipeList;
+//    }
+//
+//    public void setRecipeList(List<Recipe> recipeList) {
+//        this.recipeList = recipeList;
+//    }
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
 
     public User() {
+    }
+
+    public User(Long id, String userName, String emailAddress, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.password = password;
     }
 
     public Long getId() {
@@ -35,37 +86,38 @@ public class User {
         this.id = id;
     }
 
-    public String getusername() {
-        return username;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setusername(String username) {
-        this.username = username;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public String toString() {
-        return "Recipe{" +
+        return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
                 '}';
-    }
-
-    public Role getRole(Role role) {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public boolean getIsPublic() {
-        return isPublic;
-    }
-
-    public void setIsPublic(boolean isPublic) {
-        isPublic = isPublic;
     }
 }
 

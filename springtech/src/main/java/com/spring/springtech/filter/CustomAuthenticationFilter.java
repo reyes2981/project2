077f718dc42c39e6af8731a094034585401c21f5
noe <noe.reyes92@yahoo.com>
2022-02-01 13:58:26 -0600

    worked on attemptAuthorization - added logic that captures user input

--- springtech/src/main/java/com/spring/springtech/filter/CustomAuthenticationFilter.java
+++ springtech/src/main/java/com/spring/springtech/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.spring.springtech.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,7 +23,12 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("Username is: {}", username);
+        log.info("Password is: {}", password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
